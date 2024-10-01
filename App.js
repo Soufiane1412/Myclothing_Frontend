d44fcdceb18d4a2c8761bf1f9b15134b87986bf2
Soufiane <sfn.elmansoury@gmail.com>
2024-10-02 01:06:47 +0200

    nested navigation fully initiated

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
 const Stack = createNativeStackNavigator() 
+const Tab = createBottomTabNavigator()
+
+const TabNavigator = ()=> {
+return(
+  <Tab.Navigator screenOptions={({route }) => ({
+    tabBarIcon:({ color,size}) => {
+      let iconName='';
+
+
+      if(route.name==='Home') {
+        iconName='home-outline';
+      } else if(route.name==='Scan') {
+        iconName='search-outline';
+      } else if (route.name==='Products') {
+        iconName='shopping-bag-outline'
+      } else if (route.name==='History') {
+        iconName='archive-outline';
+      }
+
+      return <Icon name={iconName} size={size} color={color}/>;
+    },
+    tabBarActiveTintColor:'#2196f3',
+    tabBarInactiveTintColor:'gray',
+    headerShown:false,
+    })}>
+    <Tab.Screen name="Home" component={HomeScreen}/>
+    <Tab.Screen name="Scan" component={ScanScreen}/>
+    <Tab.Screen name="Products" component={ProductsScreen}/>
+    <Tab.Screen name="History" component={HistoryScreen}/>
+  </Tab.Navigator>
+);
+}
+
 
 export default function App() {
   return (
+
     <NavigationContainer>
       <Stack.Navigator screnOptions={{ headerShown:false}}>
-        <Stack.Screen name="Landing" component={LandingScreen}/>
-        <Stack.Screen name="Scan" component={ScanScreen}/>
-        <Stack.Screen name="Products" component={ProductsScreen}/>
-        <Stack.Screen name="History" component={HistoryScreen}/>
+        <Stack.Screen name="User" component={UserScreen}/>
+        <Stack.Screen name="Settings" component={SettingsScreen}/>
+        <Stack.Screen name="TabNavigator" component={TabNavigator}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
