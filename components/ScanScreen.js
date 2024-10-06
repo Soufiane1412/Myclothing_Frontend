cb6e45d2afc4feeaad1de31b9a94daaae465df64
Soufiane <sfn.elmansoury@gmail.com>
2024-10-06 17:03:47 +0200

    implementing the fetch logic

--- components/ScanScreen.js
+++ components/ScanScreen.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
 const Scan = () => {
+
+  const [isSearched, setIsSearched]=useState(false);
+  const [searchItem, setSearchItem]=useState('');
+  const [searchResult, setSearchResult]=useState([]);
+
+
+  const userQuery = searchItem;
+
+  const handleInput =()=> {
+    if (!searchItem || searchItem.length===0) {
+      return 
+    }
+    setIsSearched(true)
+    fetch(`http://localhost:3000/Products?q=${userQuery}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(error, 'error whilst fetching /Products')
+      }
+      return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      setSearchResult(data)
+    })
+    .catch(error=> {
+      console.error(error, 'Error')
+    })
+  }
+
+
+
   return (
     <View>
-      <Text>Scan Screen</Text>
+      <TextInput 
+      value={searchItem}
+      onChangeText={(value)=> setSearchItem(value)}
+      placeholder='Search items 👕...'/>
+      <Button onClick={()=> }>Search</Button>
     </View>
   );
 };
