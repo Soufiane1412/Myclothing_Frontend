899132d025f8b6939c7871c281681da384d15799
Soufiane <sfn.elmansoury@gmail.com>
2025-01-09 18:46:38 +0100

    fix: types import for navigation fix

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -1,7 +1,21 @@
 import { useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../src/types/navigation';
+
+
 
 export const ProtectedRoute = ({ children }) => {
-    const { user } = 
+    const { user } = useAuth();
+    const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+    useEffect(() => {
+      if (!user) {
+        navigation.navigate('Login')
+      }
+    }, [user, navigation]);
+
+    return user ? children : null ;
+    
 }
