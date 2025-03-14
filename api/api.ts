845130b779529e2996cefbeab96b19ab66d6481b
Soufiane <sfn.elmansoury@gmail.com>
2025-03-14 02:29:39 +0100

    inserted helper function for refresh token

--- api/api.ts
+++ api/api.ts
@@ -1,4 +1,24 @@
 // For IOS
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// Helper function for authenticated requests
+
 
 export const API_BASE_URL = 'http://192.168.1.169:8081';
 // or:
+
+export const authFetch = async (endpoint:string, options: RequestInit = {}) => {
+
+    const token = await AsyncStorage.getItem('access_token');
+
+    return fetch(`${API_BASE_URL}${endpoint}`, {
+        ... options,
+        headers: {
+            ...options.headers,
+            'Auhtorization': token ? `Bearer $ {token}`: '',
+            'Content-Type': 'application/json'
+        }
+    });
+};
+
+
